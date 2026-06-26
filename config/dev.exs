c9aba727775a64236a9d6a3a0845f072017f1c3b
Fauzi Kurniawan <kirigaya.kurniawan@gmail.com>
2026-06-26 14:41:19 +0700

    feat: refactor message and session live views, separating HTML and logic for improved maintainability

--- config/dev.exs
+++ config/dev.exs
@@ -58,12 +58,12 @@ config :wa_gate, WaGateWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/wa_gate_web/router\.ex$"E,
-      ~r"lib/wa_gate_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/wa_gate_web/router\.ex$",
+      ~r"lib/wa_gate_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
